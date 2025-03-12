b5bd86d0a708dd76ae6834ff68c2558313ceece1
tracer-ics2017 <tracer@njuics.org>
2025-03-12 00:42:26 -0700

    >  compile 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  00:42:26 up  1:50,  1 user,  load average: 7.37, 7.00, 6.20 a635c0f06a1915eb827f9db4fde161a4f6b58cb1

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -29,7 +29,7 @@ static inline make_DopHelper(I) {
 /* sign immediate */
 static inline make_DopHelper(SI) {
   assert(op->width == 1 || op->width == 4);
-
+  //首先确保操作数宽度是合法的（1或4字节）
   op->type = OP_TYPE_IMM;
 
   /* TODO: Use instr_fetch() to read `op->width' bytes of memory
@@ -38,8 +38,13 @@ static inline make_DopHelper(SI) {
    *
    op->simm = ???
    */
-  TODO();
-
+  // TODO();
+  //使用instr_fetch()从eip指向的内存读取指定宽度的数据
+  //将读取的数据作为有符号立即数存储在op->simm中
+  //simm表示signed immediate，有符号立即数
+  op->simm = instr_fetch(eip, op->width);
+  
+  // 根据操作数宽度进行符号扩展，使用RTL指令将立即数加载到操作数的val字段中
   rtl_li(&op->val, op->simm);
 
 #ifdef DEBUG
--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -130,7 +130,9 @@ opcode_entry opcode_table [512] = {
   /* 0xdc */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xe0 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xe4 */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /*Pa2.1 begin*/
   /* 0xe8 */	IDEX(J, call), EMPTY, EMPTY, EMPTY,
+  /*Pa2.1 end*/
   /* 0xec */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xf0 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xf4 */	EMPTY, EMPTY, IDEXW(E, gp3, 1), IDEX(E, gp3),
