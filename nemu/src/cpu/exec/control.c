19da8d5dc1a46ae75fcafd86ce572250bc819836
tracer-ics2017 <tracer@njuics.org>
2025-03-12 01:12:11 -0700

    >  compile 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  01:12:10 up  2:20,  1 user,  load average: 7.74, 7.45, 7.13 c8126cf5f3da6b42c91f64286c3cc73e69dda835

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -24,10 +24,17 @@ make_EHelper(jmp_rm) {
 }
 
 make_EHelper(call) {
+  /*Pa1 Begin*/
   // the target address is calculated at the decode stage
-  TODO();
+  // 保存下一条指令的地址到栈中
+  rtl_push(&decoding.seq_eip);
+  
+  // 设置跳转标志和目标地址
+  decoding.is_jmp = 1;
+  // 注意：decoding.jmp_eip已经在译码阶段设置好了，不需要我们计算
 
   print_asm("call %x", decoding.jmp_eip);
+    /*Pa1 end*/
 }
 
 make_EHelper(ret) {
