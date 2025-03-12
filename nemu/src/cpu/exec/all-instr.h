bb78f45db9cbec671b7a14448b23af3ddbc3d5f4
2212478-Liu Hao Ze <2212478@mail.nankai.edu.cn>
2025-03-12 03:33:17 -0700

    PA2.1  完成jump，call，sub指令并进行测试

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -7,7 +7,16 @@ make_EHelper(operand_size);
 make_EHelper(inv);
 make_EHelper(nemu_trap);
 
+
+
 /*Pa2.1 begin*/
-// 添加sub指令的声明
+// 添加指令的声明
+// Group 1 instructions
+make_EHelper(add);
+make_EHelper(or);
+make_EHelper(sbb);
+make_EHelper(and);
 make_EHelper(sub);
+make_EHelper(xor);
+make_EHelper(cmp);
 /*Pa2.1 end*/
--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -41,8 +41,14 @@ static make_EHelper(name) { \
 
 /* 0x80, 0x81, 0x83 */
 make_group(gp1,
-    EMPTY, EMPTY, EMPTY, EMPTY,
-    EMPTY, EMPTY, EMPTY, EMPTY)
+    EX(add),  /* 000 */ // ADD Eb/Ev, Ib
+    EX(or),   /* 001 */ // OR  Eb/Ev, Ib
+    EMPTY,    /* 010 */ // ADC Eb/Ev, Ib
+    EX(sbb),  /* 011 */ // SBB Eb/Ev, Ib
+    EX(and),  /* 100 */ // AND Eb/Ev, Ib
+    EX(sub),  /* 101 */ // SUB Eb/Ev, Ib
+    EX(xor),  /* 110 */ // XOR Eb/Ev, Ib
+    EX(cmp))  /* 111 */ // CMP Eb/Ev, Ib
 
   /* 0xc0, 0xc1, 0xd0, 0xd1, 0xd2, 0xd3 */
 make_group(gp2,
