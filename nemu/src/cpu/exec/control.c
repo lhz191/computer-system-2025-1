e6775fa251ceed9ce76946676babc49971372f2b
2212478-Liu Hao Ze <2212478@mail.nankai.edu.cn>
2025-03-13 05:04:37 -0700

    0x90 nop指令

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -61,7 +61,8 @@ make_EHelper(call_rm) {
   print_asm("call *%s", id_dest->str);
 }
 
-make_EHelper(nop) {
-  // nop指令不做任何操作
-  print_asm("nop");
-}
+/*Pa2.1 在special.c文件中已被定义*/
+// make_EHelper(nop) {
+//   // nop指令不做任何操作
+//   print_asm("nop");
+// }
