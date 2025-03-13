f54adcfdf944fe56e849d8d0ebb3738da3de9dda
2212478-Liu Hao Ze <2212478@mail.nankai.edu.cn>
2025-03-13 00:59:15 -0700

    Pa2.1 0x84: je rel32

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -222,7 +222,8 @@ opcode_entry opcode_table [512] = {
   /* 0x78 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x7c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x80 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x84 */	EMPTY, EMPTY, EMPTY, EMPTY,
+  //Pa2.1 0x84: je rel32 (jump if equal)
+  /* 0x84 */	IDEX(J, jcc), EMPTY, EMPTY, EMPTY, 
   /* 0x88 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x8c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x90 */	EMPTY, EMPTY, EMPTY, EMPTY,
