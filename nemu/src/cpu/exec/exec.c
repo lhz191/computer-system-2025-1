bc6011617623a8a51ecaf29b5eee8f1cd880dd9b
2212478-Liu Hao Ze <2212478@mail.nankai.edu.cn>
2025-03-13 04:26:44 -0700

    Pa2.1 0x58-5f pop %ebp指令

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -141,10 +141,10 @@ opcode_entry opcode_table [512] = {
   // 0x55 是 push ebp
   /* 0x54 */	IDEX(r, push), IDEX(r, push), IDEX(r, push), IDEX(r, push),
   /*Pa2.1 End*/
-  /* 0x58 */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0x58 */	IDEX(r, pop), IDEX(r, pop), IDEX(r, pop), IDEX(r, pop),
   
-  //Pa2.1 0x5d pop %ebp指令。
-  /* 0x5c */	EMPTY, IDEX(r, pop), EMPTY, EMPTY,  
+  //Pa2.1 0x58-5f pop %ebp指令。
+  /* 0x5c */	IDEX(r, pop), IDEX(r, pop), IDEX(r, pop), IDEX(r, pop),  
   /* 0x60 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x64 */	EMPTY, EMPTY, EX(operand_size), EMPTY,
   /*Pa2.1 0x68:push imm32*/
