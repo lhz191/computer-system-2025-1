996c858f0e8e08079ee59b040d512892a4f6a0f5
tracer-ics2017 <tracer@njuics.org>
2025-03-13 03:32:27 -0700

    >  compile 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  03:32:27 up 24 min,  1 user,  load average: 1.78, 0.89, 0.42 56340019a38ddcb510a6042f41bdfc4fe2ed6e60

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -33,6 +33,8 @@ make_EHelper(inc);  // 添加inc指令声明
 make_EHelper(dec);  // 添加dec指令声明
 make_EHelper(neg);  // 添加neg指令声明
 make_EHelper(ret);  // 添加ret指令声明
+make_EHelper(jmp);  // 添加jmp指令声明
+
 /*Pa2.1 end*/
 
 
