d4b801581e06fc926b058df47c32d3c840c9eb98
tracer-ics2017 <tracer@njuics.org>
2025-03-17 01:46:18 -0700

    >  compile 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  01:46:18 up  4:46,  1 user,  load average: 12.48, 11.33, 8.42 3463f50fb547dd7b641c82c3f3de8b95da095160

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -24,24 +24,11 @@ make_EHelper(jmp_rm) {
 }
 
 make_EHelper(call) {
-  // 1. 保存下一条指令的地址到栈中
+  // the target address is calculated at the decode stage.
   rtl_push(&decoding.seq_eip);
-  
-  // 2. 计算跳转目标地址
-  decoding.jmp_eip = decoding.seq_eip + id_dest->imm;
-  
-  // 3. 设置跳转标志位
+  //rtl_push(&cpu.eip);
+  cpu.eip=decoding.jmp_eip;
   decoding.is_jmp = 1;
-  
-  // 调试信息
-  printf("当前eip: 0x%x\n", cpu.eip);
-  printf("下一条指令地址: 0x%x\n", decoding.seq_eip);
-  printf("偏移量: 0x%x\n", id_dest->imm); 
-  printf("跳转目标: 0x%x\n", decoding.jmp_eip);
-  
-  // 不要直接设置cpu.eip
-  // cpu.eip = decoding.jmp_eip; <- 删除这行
-
   print_asm("call %x", decoding.jmp_eip);
 }
 make_EHelper(ret) {
