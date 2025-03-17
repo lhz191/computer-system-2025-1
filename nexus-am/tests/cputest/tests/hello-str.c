29b737a87c667bef5dc82d3daf8b3334df84ef91
tracer-ics2017 <tracer@njuics.org>
2025-03-16 23:59:42 -0700

    >  run 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  23:59:42 up  3:39,  1 user,  load average: 6.01, 6.00, 5.18 b1293c791884c5077b9778f548c8a47990913d3

--- nexus-am/tests/cputest/tests/hello-str.c
+++ nexus-am/tests/cputest/tests/hello-str.c
@@ -3,7 +3,8 @@
 char buf[128];
 
 int main() {
-
+	printf(buf, "%s", "Hello world!\n");
+	nemu_assert(strcmp(buf, "Hello world!\n") == 0);
 
 	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
 	nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
