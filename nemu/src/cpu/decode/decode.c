4d90befd4784a0a2a2aabf6dc279e524b21d3e4a
tracer-ics2017 <tracer@njuics.org>
2025-03-17 02:14:05 -0700

    >  compile 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  02:14:05 up  5:14,  1 user,  load average: 19.73, 18.92, 16.03 96504037527541534209a9bea997d64e6279e4e2

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -53,7 +53,7 @@ static inline make_DopHelper(SI) {
   op->simm = (int32_t)imm;
   // 根据操作数宽度进行符号扩展，使用RTL指令将立即数加载到操作数的val字段中
   rtl_li(&op->val, op->simm);
-  // printf("指令大小: %d字节\n", op->width);
+  printf("指令大小: %d字节\n", op->width);
   printf("指令原始偏移量: 0x%x\n", op->simm);
 #ifdef DEBUG
   snprintf(op->str, OP_STR_SIZE, "$0x%x", op->simm);
