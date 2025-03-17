dd9a8e43f3beada3a379b4597ca2477022c3889d
tracer-ics2017 <tracer@njuics.org>
2025-03-17 01:43:17 -0700

    >  compile 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  01:43:17 up  4:43,  1 user,  load average: 11.18, 10.47, 7.57 2c8b44bcfb815794b48c37f6d7799d0325d72a95

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -28,7 +28,7 @@ make_EHelper(call) {
   rtl_push(&decoding.seq_eip);
   
   // 2. 计算跳转目标地址
-  decoding.jmp_eip = decoding.seq_eip + id_dest->val;
+  decoding.jmp_eip = decoding.seq_eip + id_dest->imm;
   
   // 3. 设置跳转标志位
   decoding.is_jmp = 1;
@@ -36,7 +36,7 @@ make_EHelper(call) {
   // 调试信息
   printf("当前eip: 0x%x\n", cpu.eip);
   printf("下一条指令地址: 0x%x\n", decoding.seq_eip);
-  printf("偏移量: 0x%x\n", id_dest->val); 
+  printf("偏移量: 0x%x\n", id_dest->imm); 
   printf("跳转目标: 0x%x\n", decoding.jmp_eip);
   
   // 不要直接设置cpu.eip
@@ -44,7 +44,6 @@ make_EHelper(call) {
 
   print_asm("call %x", decoding.jmp_eip);
 }
-
 make_EHelper(ret) {
   // 从栈中弹出返回地址
   rtl_pop(&t0);
