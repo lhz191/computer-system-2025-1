8f1b341ddd411059f8c96d057053f60fc965adc5
2212478-Liu Hao Ze <2212478@mail.nankai.edu.cn>
2025-03-19 01:21:25 -0700

    Pa2.3 实现in和out函数

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -45,7 +45,10 @@ uint32_t pio_read(ioaddr_t, int);
 void pio_write(ioaddr_t, int, uint32_t);
 
 make_EHelper(in) {
-  TODO();
+  // TODO();
+  // 从设备端口读取数据
+  rtl_li(&t0, pio_read(id_src->val, id_dest->width));
+  operand_write(id_dest, &t0);
 
   print_asm_template2(in);
 
@@ -55,7 +58,8 @@ make_EHelper(in) {
 }
 
 make_EHelper(out) {
-  TODO();
+  // 向设备端口写入数据
+  pio_write(id_dest->val, id_src->width, id_src->val);
 
   print_asm_template2(out);
 
