357ed092a2a9b00d6b1579b2ea93bf2eb8c284f5
2212478-Liu Hao Ze <2212478@mail.nankai.edu.cn>
2025-03-19 05:16:51 -0700

    Pa2.3 为了运行litenes，补充了一堆指令的opcode_table

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -10,12 +10,28 @@
 uint8_t pmem[PMEM_SIZE];
 
 /* Memory accessing interfaces */
+/*Pa2.3 读取内存*/
+// uint32_t paddr_read(paddr_t addr, int len) {
+//   return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+// }
 
 uint32_t paddr_read(paddr_t addr, int len) {
+  int mmio_id = is_mmio(addr);
+  if (mmio_id != -1) {
+    return mmio_read(addr,len,mmio_id);
+  }
   return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
 }
 
+// void paddr_write(paddr_t addr, int len, uint32_t data) {
+//   memcpy(guest_to_host(addr), &data, len);
+// }
+
 void paddr_write(paddr_t addr, int len, uint32_t data) {
+  int mmio_id = is_mmio(addr);
+  if (mmio_id != -1) {
+    mmio_write(addr,len,data,mmio_id);
+  }
   memcpy(guest_to_host(addr), &data, len);
 }
 
