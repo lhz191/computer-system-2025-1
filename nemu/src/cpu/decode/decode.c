56b2bfd033e566104f040ee290549eebf6329c7e
tracer-ics2017 <tracer@njuics.org>
2025-03-19 01:52:17 -0700

    >  compile 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  01:52:17 up 16:23,  1 user,  load average: 19.02, 17.20, 14.72 8ed004f8682f945645a0dedab586d24b4c9f68a1

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -43,7 +43,7 @@ static inline make_DopHelper(SI) {
   //将读取的数据作为有符号立即数存储在op->simm中
   //simm表示signed immediate，有符号立即数
   uint32_t imm = instr_fetch(eip, op->width);
-    // printf("指令原始偏移量: 0x%x\n", imm);
+    printf("指令原始偏移量: 0x%x\n", imm);
   // if (op->width == 1) {
   //   // 对8位立即数进行符号扩展
   //   op->simm = (int8_t)imm;
@@ -51,13 +51,13 @@ static inline make_DopHelper(SI) {
   //   op->simm = (int32_t)imm;
   // }
   rtl_sext(&imm, &imm, op->width);
-  // printf("指令原始偏移量: 0x%x\n", imm);
+  printf("指令原始偏移量: 0x%x\n", imm);
 
   op->simm = (int32_t)imm;
   // 根据操作数宽度进行符号扩展，使用RTL指令将立即数加载到操作数的val字段中
   rtl_li(&op->val, op->simm);
-  // printf("指令大小: %d字节\n", op->width);
-  // printf("指令原始偏移量: 0x%x\n", imm);
+  printf("指令大小: %d字节\n", op->width);
+  printf("指令原始偏移量: 0x%x\n", imm);
 #ifdef DEBUG
   snprintf(op->str, OP_STR_SIZE, "$0x%x", op->simm);
 #endif
@@ -280,13 +280,13 @@ make_DHelper(J) {
   decode_op_SI(eip, id_dest, false);
   
   // 添加调试信息
-  // printf("J解码器 - 当前eip: 0x%x\n", *eip);
-  // printf("J解码器 - 偏移量: 0x%x (%d)\n", id_dest->simm, id_dest->simm);
+  printf("J解码器 - 当前eip: 0x%x\n", *eip);
+  printf("J解码器 - 偏移量: 0x%x (%d)\n", id_dest->simm, id_dest->simm);
   
   // the target address can be computed in the decode stage
   decoding.jmp_eip = id_dest->simm + *eip;
   
-  // printf("J解码器 - 计算出的跳转目标: 0x%x\n", decoding.jmp_eip);
+  printf("J解码器 - 计算出的跳转目标: 0x%x\n", decoding.jmp_eip);
 }
 
 make_DHelper(push_SI) {
