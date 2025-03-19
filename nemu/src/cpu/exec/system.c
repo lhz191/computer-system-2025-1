802022202f55aa52f9eb52e56925abaa0214bb6e
tracer-ics2017 <tracer@njuics.org>
2025-03-19 02:40:52 -0700

    >  compile 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  02:40:52 up 17:12,  1 user,  load average: 24.68, 24.37, 23.32 b55e18b4148c91a7315f03ecc70fb95fe0d5b959

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -45,8 +45,6 @@ uint32_t pio_read(ioaddr_t, int);
 void pio_write(ioaddr_t, int, uint32_t);
 
 make_EHelper(in) {
-  // TODO();
-  // 从设备端口读取数据
   rtl_li(&t0, pio_read(id_src->val, id_dest->width));
   operand_write(id_dest, &t0);
 
@@ -58,12 +56,11 @@ make_EHelper(in) {
 }
 
 make_EHelper(out) {
-  // 向设备端口写入数据
   pio_write(id_dest->val, id_src->width, id_src->val);
-
   print_asm_template2(out);
 
 #ifdef DIFF_TEST
   diff_test_skip_qemu();
 #endif
 }
+
