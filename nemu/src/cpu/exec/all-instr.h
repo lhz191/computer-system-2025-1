d9d2c783badf852e33af2ce2b54929779694d5a5
tracer-ics2017 <tracer@njuics.org>
2025-04-01 04:41:43 -0700

    >  compile 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  04:41:43 up  1:09,  1 user,  load average: 0.00, 0.12, 0.20 a46fc444e1083a458341346cc497782359f1c1b8

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -81,110 +81,14 @@ make_EHelper(cwtl);
 /*Pa2.3 End*/
 
 
-/*Pa3.2 Begin*/
+/*Pa3.1 Begin*/
 make_EHelper(int);
 make_EHelper(call_rm);
 make_EHelper(lidt);
 make_EHelper(pusha);
 make_EHelper(popa);
-make_EHelper(iret)
-/*Pa3.2 End*/
-
-
-
-
-
-#include "cpu/exec.h"
-
-make_EHelper(mov);
-
-make_EHelper(operand_size);
-
-make_EHelper(inv);
-make_EHelper(nemu_trap);
-
-
-
-make_EHelper(call);
-make_EHelper(sub);
-make_EHelper(xor);
-make_EHelper(push);
-make_EHelper(pop);  
-make_EHelper(ret);  
-
-
-
-
-/*Pa2.1 Pa2.2 begin*/
-// 添加sub指令的声明
-// Group 1 instructions
-make_EHelper(add);
-make_EHelper(or);
-make_EHelper(cmp);
-
-
-make_EHelper(and);
-make_EHelper(sbb);
-
-
-
-make_EHelper(lea);
-make_EHelper(xor);  // 添加xor指令声明
-make_EHelper(movsx);  // 添加movsx指令声明
-make_EHelper(test);  // 添加test指令声明
-make_EHelper(jcc);  // 添加jcc指令声明
-make_EHelper(cmp);  // 添加cmp指令声明
-make_EHelper(inc);  // 添加inc指令声明
-make_EHelper(dec);  // 添加dec指令声明
-make_EHelper(neg);  // 添加neg指令声明
-make_EHelper(jmp);  // 添加jmp指令声明
-
-make_EHelper(jmp_rm);  // 添加jmp_rm声明
-
-make_EHelper(leave);  // 添加leave指令声明
-make_EHelper(nop);  // 添加nop指令声明
-
-make_EHelper(setcc);  // 添加setcc指令声明
-
-make_EHelper(movzx);  // 添加movzx指令声明
-
-make_EHelper(adc);  // 添加adc指令声明
-
-make_EHelper(rol);  // 循环左移
-make_EHelper(shl);  // 逻辑左移
-make_EHelper(shr);  // 逻辑右移
-make_EHelper(sar);  // 算术右移
-
-
-
-
-// gp3 组指令声明
-make_EHelper(not);   // NOT r/m
-make_EHelper(neg);   // NEG r/m
-make_EHelper(mul);   // MUL r/m (无符号乘法)
-make_EHelper(imul1); // IMUL r/m (有符号乘法)
-make_EHelper(imul2); //
-make_EHelper(div);   // DIV r/m (无符号除法)
-make_EHelper(idiv);  // IDIV r/m (有符号除法)
-
-make_EHelper(cltd);  // CDQ指令的另一个名字是CLTD
-/*Pa2.1 Pa2.2 end*/
-
-/*Pa2.3 Begin*/
-make_EHelper(in);
-make_EHelper(out);
-make_EHelper(cwtl);
-/*Pa2.3 End*/
-
-
-/*Pa3.2 Begin*/
-make_EHelper(int);
-make_EHelper(call_rm);
-make_EHelper(lidt);
-make_EHelper(pusha);
-make_EHelper(popa);
-make_EHelper(iret)
-/*Pa3.2 End*/
+make_EHelper(iret);
+/*Pa3.1 End*/
 
 
 
--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -162,7 +162,7 @@ opcode_entry opcode_table [512] = {
   
   //Pa2.1 0x58-5f pop %ebp指令。
   /* 0x5c */	IDEX(r, pop), IDEX(r, pop), IDEX(r, pop), IDEX(r, pop),  
-  /* 0x60 */	EX(pusha), EX(popa), EMPTY, EMPTY,
+  /* 0x60 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x64 */	EMPTY, EMPTY, EX(operand_size), EMPTY,
   /*Pa2.1 0x68:push imm32*/
   /*Pa2.1 0x6a：push imm8*/
@@ -201,7 +201,7 @@ opcode_entry opcode_table [512] = {
   /* 0xc0 */	IDEXW(gp2_Ib2E, gp2, 1), IDEX(gp2_Ib2E, gp2), EMPTY, EX(ret),
   /* 0xc4 */	EMPTY, EMPTY, IDEXW(mov_I2E, mov, 1), IDEX(mov_I2E, mov),
   /* 0xc8 */	EMPTY, EX(leave), EMPTY, EMPTY,
-  /* 0xcc */	EMPTY, IDEXW(I, int, 1), EMPTY, EX(iret),
+  /* 0xcc */	EMPTY, IDEXW(I, int, 1), EMPTY, EMPTY,
   /* 0xd0 */	IDEXW(gp2_1_E, gp2, 1), IDEX(gp2_1_E, gp2), IDEXW(gp2_cl2E, gp2, 1), IDEX(gp2_cl2E, gp2),
   /* 0xd4 */	EMPTY, EMPTY, EX(nemu_trap), EMPTY,
   /* 0xd8 */	EMPTY, EMPTY, EMPTY, EMPTY,
