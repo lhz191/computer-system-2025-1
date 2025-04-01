04f6fafd4891dc81fca6b96d9d984ebe241ce347
2212478-Liu Hao Ze <2212478@mail.nankai.edu.cn>
2025-04-01 04:23:49 -0700

    Pa3.1 实现sysnone系统调用

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,10 +2,16 @@
 #include "syscall.h"
 
 _RegSet* do_syscall(_RegSet *r) {
-  uintptr_t a[4];
-  a[0] = SYSCALL_ARG1(r);
+  uintptr_t a[4]; // 用于存储系统调用参数
+  a[0] = SYSCALL_ARG1(r);// 获取系统调用号，根据之前的实现，存储在eax中
 
   switch (a[0]) {
+    case SYS_none:{
+    //SYS_none 是一个系统调用的标识符
+    //通常用于表示一个不执行任何操作的系统调用。返回值置1，return null
+      SYSCALL_ARG1(r) = 1;
+      break;
+    }
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
