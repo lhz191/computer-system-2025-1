03bd1dbd0274ca620f0d4e86f17a8fe052f768a8
2212478-Liu Hao Ze <2212478@mail.nankai.edu.cn>
2025-04-01 03:45:42 -0700

    修改_RegSet结构体定义

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -1,7 +1,7 @@
 #include "cpu/exec.h"
 #include "memory/mmu.h"
 
-//Pa3.2
+//Pa3.1
 void raise_intr(uint8_t NO, vaddr_t save_addr) {
   /* 根据i386中断机制实现中断触发：
    * 1. 将EFLAGS, CS, EIP压入栈
@@ -22,7 +22,7 @@ void raise_intr(uint8_t NO, vaddr_t save_addr) {
   // 3. 读取门描述符中的offset域
   rtl_li(&t0, vaddr_read(gate_addr,2));
   rtl_li(&t1, vaddr_read(gate_addr + 4, 4));
-  
+
   // 4. 跳转到目标地址
   uint32_t target = (t1 & 0xffff0000) | (t0& 0xffff);
   decoding.jmp_eip = target;
--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -13,10 +13,10 @@ struct _RegSet {
   uintptr_t eip, cs, eflags;
 };
 
-#define SYSCALL_ARG1(r) 0
-#define SYSCALL_ARG2(r) 0
-#define SYSCALL_ARG3(r) 0
-#define SYSCALL_ARG4(r) 0
+#define SYSCALL_ARG1(r) ((r)->eax)  // 系统调用号
+#define SYSCALL_ARG2(r) ((r)->ebx)  // 第一个参数
+#define SYSCALL_ARG3(r) ((r)->ecx)  // 第二个参数
+#define SYSCALL_ARG4(r) ((r)->edx)  // 第三个参数
 
 #ifdef __cplusplus
 extern "C" {
