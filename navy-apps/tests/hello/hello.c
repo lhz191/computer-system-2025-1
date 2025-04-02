deafb74f0f14067e95318590bcfce9f77e77dddf
tracer-ics2017 <tracer@njuics.org>
2025-04-01 21:50:21 -0700

    >  run 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  21:50:21 up  4:52,  1 user,  load average: 22.82, 20.91, 19.64 ab1f4efdd45b95e5157fac5911a65196b85682fc

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -4,12 +4,5 @@
 int main() {
   write(1, "Hello World!\n", 13);
   int i = 2;
-  volatile int j = 0;
-  while (1) {
-    j ++;
-    printf("Hello World for the %dth time\n", i ++);
-     j = 0;
-    break;
-  }
   return 0;
 }
