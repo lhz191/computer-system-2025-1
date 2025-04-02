3d0092c6100f7534ddb68a37e701cae8e11e1363
tracer-ics2017 <tracer@njuics.org>
2025-04-01 19:36:39 -0700

    >  compile 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  19:36:39 up  2:39,  1 user,  load average: 7.70, 7.96, 7.87 901921b6b5cfd5a024216afba61167d91ee2ee19

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -1,88 +1,47 @@
+
 #include "cpu/exec.h"
-#include "common.h"
 
-// 函数声明，放在文件开头
-void raise_intr(uint8_t NO, vaddr_t save_addr);
-void diff_test_skip_qemu();
-void diff_test_skip_nemu();
-
-//Pa3.2 实现lidt指令
-make_EHelper(lidt) {
-  rtl_li(&t0, id_dest->addr);
-  
-  // 读取limit和base
-  cpu.idtr.limit = vaddr_read(t0, 2);
-  cpu.idtr.base = vaddr_read(t0 + 2, 4);
-  
-  print_asm_template1(lidt);
-}
-
-make_EHelper(mov_r2cr) {
-  TODO();
-
-  print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
-}
-
-make_EHelper(mov_cr2r) {
-  TODO();
-
-  print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
-
-#ifdef DIFF_TEST
-  diff_test_skip_qemu();
-#endif
-}
-
-
-//Pa3.2 实现中断函数
-make_EHelper(int) {
-  raise_intr(id_dest->val, decoding.seq_eip);
-  print_asm("int %s", id_dest->str);
-
-#ifdef DIFF_TEST
-  diff_test_skip_nemu();
-#endif
-}
-//Pa3.1 实现iret函数
-make_EHelper(iret) {
-  // 从栈中弹出 EIP, CS, EFLAGS
-  rtl_pop(&decoding.jmp_eip);        // 恢复 EIP
-  rtl_pop(&cpu.cs);         // 恢复 CS
-  rtl_pop(&cpu.eflags.val); // 恢复 EFLAGS
-
-  // 设置 decoding.is_jmp 使 CPU 跳转到弹出的 EIP
+make_EHelper(jmp) {
+  // the target address is calculated at the decode stage
   decoding.is_jmp = 1;
 
-  print_asm("iret");
+  print_asm("jmp %x", decoding.jmp_eip);
 }
 
-uint32_t pio_read(ioaddr_t, int);
-void pio_write(ioaddr_t, int, uint32_t);
+make_EHelper(jcc) {
+  // the target address is calculated at the decode stage
+  uint8_t subcode = decoding.opcode & 0xf;
+  rtl_setcc(&t2, subcode);
+  decoding.is_jmp = t2;
 
-make_EHelper(in) {
-  // TODO();
-  // 从设备端口读取数据
-    // 从设备端口读取数据
-  // uint32_t port_value = pio_read(id_src->val, id_dest->width);
-  // printf("从端口 0x%x 读取值: 0x%x (宽度: %d)\n", id_src->val, port_value, id_dest->width);
-  rtl_li(&t0, pio_read(id_src->val, id_dest->width));
-  operand_write(id_dest, &t0);
-
-  print_asm_template2(in);
-
-#ifdef DIFF_TEST
-  diff_test_skip_qemu();
-#endif
+  print_asm("j%s %x", get_cc_name(subcode), decoding.jmp_eip);
 }
 
-make_EHelper(out) {
-  // printf("向端口 0x%x 写入值: 0x%x (宽度: %d)\n", id_dest->val, id_src->val, id_src->width);
-  // 向设备端口写入数据
-  pio_write(id_dest->val, id_src->width, id_src->val);
+make_EHelper(jmp_rm) {
+  decoding.jmp_eip = id_dest->val;
+  decoding.is_jmp = 1;
 
-  print_asm_template2(out);
+  print_asm("jmp *%s", id_dest->str);
+}
 
-#ifdef DIFF_TEST
-  diff_test_skip_qemu();
-#endif
+make_EHelper(call) {
+  // the target address is calculated at the decode stage
+  // eip压栈 设置跳转目标地址 设置跳转标记
+  rtl_push(eip);
+  rtl_addi(&decoding.jmp_eip,eip,id_dest->val);//decode.h #define id_dest (&decoding.dest)
+  decoding.is_jmp = 1;
+  print_asm("call %x", decoding.jmp_eip);
+}
+
+make_EHelper(ret) {
+  rtl_pop(&decoding.jmp_eip);
+  decoding.is_jmp = 1;
+  print_asm("ret");
+}
+
+make_EHelper(call_rm) {
+  rtl_push(eip);
+  decoding.jmp_eip=id_dest->val;
+  decoding.is_jmp = 1;
+  print_asm("call *%s", id_dest->str);
 }
