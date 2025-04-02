16c98dc89175b187c2575c7edd1a236eab2d2ae0
2212478-Liu Hao Ze <2212478@mail.nankai.edu.cn>
2025-04-02 05:12:55 -0700

    Pa3.2 实现SYS_brk系统调用, 并在用户层实现_sbrk()

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,6 +6,11 @@ ssize_t fs_write(int fd, const void *buf, size_t len);
 // off_t fs_lseek(int fd, off_t offset, int whence);
 // int fs_close(int fd);
 
+// 为了强制单字符输出，sys_brk总是返回失败
+static int sys_brk(uintptr_t addr) {
+  // 返回-1表示失败，这样会迫使printf()逐字符输出
+  return -1;
+}
 
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4]; // 用于存储系统调用参数
@@ -37,6 +42,13 @@ _RegSet* do_syscall(_RegSet *r) {
       // } 
       SYSCALL_ARG1(r) = fs_write(fd,buf,len);
       return NULL;
+      break;
+    }
+    case SYS_brk: {
+      // 获取新的program break地址
+      uintptr_t addr = SYSCALL_ARG2(r);
+      // 调用sys_brk处理
+      SYSCALL_ARG1(r) = sys_brk(addr);
       break;
     }
     default: panic("Unhandled syscall ID = %d", a[0]);
