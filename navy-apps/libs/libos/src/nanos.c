7f4a69e00d276f389b268c194c193e62b6945108
tracer-ics2017 <tracer@njuics.org>
2025-04-01 17:16:12 -0700

    >  run 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  17:16:12 up 18 min,  1 user,  load average: 0.39, 0.25, 0.21 16d702fdaf3928cc379f1133ae909b25d20adac7

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -26,7 +26,7 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 /*Pa3.2 write系统调用*/
 int _write(int fd, void *buf, size_t count){
-  return _syscall_(SYS_write, fd, buf, count);
+  return _syscall_(SYS_write, fd, (uintptr_t)buf, count);
 }
 
 void *_sbrk(intptr_t increment){
