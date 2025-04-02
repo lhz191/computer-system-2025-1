ffcff9ae9e726d990dd3e46dcbf3e743c980c79f
tracer-ics2017 <tracer@njuics.org>
2025-04-01 19:32:23 -0700

    >  compile 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  19:32:23 up  2:34,  1 user,  load average: 7.39, 7.58, 7.72 30ce9121855c9784c5183e907e36431472d954e2

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -81,14 +81,16 @@ make_EHelper(cwtl);
 /*Pa2.3 End*/
 
 
-/*Pa3.1 Begin*/
+/*Pa3.2 Begin*/
 make_EHelper(int);
 make_EHelper(call_rm);
 make_EHelper(lidt);
 make_EHelper(pusha);
 make_EHelper(popa);
 make_EHelper(iret);
-/*Pa3.1 End*/
+make_EHelper(mov_r2cr);
+make_EHelper(mov_cr2r);
+/*Pa3.2 End*/
 
 
 
