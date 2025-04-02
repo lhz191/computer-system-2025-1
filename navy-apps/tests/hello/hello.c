584dfa61014d4d6c7edeadffd058eee265c109f6
tracer-ics2017 <tracer@njuics.org>
2025-04-01 22:45:53 -0700

    >  run 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  22:45:53 up  5:48,  1 user,  load average: 28.72, 29.67, 27.67 e5eb6a89ec04a217d0ed1749f011dbb8b891b598

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 
 int main() {
-  printf("111");
+  write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
-printf("222");
   while (1) {
     j ++;
     if (j == 10000) {
