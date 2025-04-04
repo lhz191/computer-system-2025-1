b61afd1bc2047c6c479ce8f8bddd387d4b18e10e
tracer-ics2017 <tracer@njuics.org>
2025-04-03 19:47:28 -0700

    >  run 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  19:47:28 up  1:16,  1 user,  load average: 0.59, 2.29, 2.71 44e0ef4fea7431a7b3f2aceda06e5f7ee07567c1

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -10,19 +10,18 @@ static const char *keyname[256] __attribute__((used)) = {
 
 size_t events_read(void *buf, size_t len) {
   int key = _read_key();
-  bool is_down = false;
+  bool down = false;
   if(key & 0x8000 ) {
     key ^= 0x8000;
-    is_down = true;
+    down = true;
   }
   if(key == _KEY_NONE) {
     uint32_t ut = _uptime();
     sprintf(buf, "t %d\n", ut);
   } 
   else {
-    sprintf(buf, "%s %s\n", is_down ? "kd" : "ku", keyname[key]);
+    sprintf(buf, "%s %s\n", down ? "kd" : "ku", keyname[key]);
   }
-  
   return strlen(buf);
 }
 
