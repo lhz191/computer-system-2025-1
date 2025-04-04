9763304f67402df2d51d1a0efeed2342f172c1bf
tracer-ics2017 <tracer@njuics.org>
2025-04-03 21:12:45 -0700

    >  compile 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  21:12:45 up  2:38,  1 user,  load average: 3.44, 2.66, 1.84 3db00676138f8fdf2457a702407cfe416d86e678

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -28,13 +28,20 @@ uint32_t paddr_read(paddr_t addr, int len) {
 //   memcpy(guest_to_host(addr), &data, len);
 // }
 
-void paddr_write(paddr_t addr, int len, uint32_t data) {
-  int mmio_id = is_mmio(addr);
-  if (mmio_id != -1) {
-    mmio_write(addr,len,data,mmio_id);
-  }
-  memcpy(guest_to_host(addr), &data, len);
-}
+   void paddr_write(paddr_t addr, int len, uint32_t data) {
+     // 保护低地址区域
+     if (addr < 0x1000) {
+       Log("Warning: Attempt to write to low memory address 0x%x, ignored", addr);
+       return;  // 忽略对低地址的写入
+     }
+     
+     int mmio_id = is_mmio(addr);
+     if (mmio_id != -1) {
+       mmio_write(addr,len,data,mmio_id);
+       return;  // 已在mmio_write处理，不需要再写入主内存
+     }
+     memcpy(guest_to_host(addr), &data, len);
+   }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
   return paddr_read(addr, len);
