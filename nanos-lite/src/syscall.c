be5f75c36b440516a05b6f47c8aabfc81a4af8f3
tracer-ics2017 <tracer@njuics.org>
2025-04-13 07:09:04 -0700

    >  run 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  07:09:04 up 28 min,  1 user,  load average: 4.55, 3.71, 2.25 46e3bd9b53cbfbe0f6337a5bb98fe98d43de444f

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,86 +1,109 @@
 #include "common.h"
 #include "syscall.h"
+
 int fs_open(const char *pathname, int flags, int mode);
 ssize_t fs_read(int fd, void *buf, size_t len);
 ssize_t fs_write(int fd, const void *buf, size_t len);
 off_t fs_lseek(int fd, off_t offset, int whence);
 int fs_close(int fd);
+extern int mm_brk(uint32_t);
 
-// 为了强制单字符输出，sys_brk总是返回失败
-static int sys_brk(uintptr_t addr) {
-  // 返回0表示失败，这样会迫使printf()逐字符输出
-  return 0;
+_RegSet* sys_none(_RegSet *r){
+  SYSCALL_ARG1(r) = 1;
+  return NULL;
+}
+
+_RegSet* sys_write(_RegSet *r){
+  int fd = SYSCALL_ARG2(r);
+  char* buf = (char*) SYSCALL_ARG3(r);
+  int len = SYSCALL_ARG4(r);
+
+  // if(fd == 1 || fd == 2){
+  //   for(int i = 0; i < len; i++){
+  //     _putc(buf[i]);
+  //   }
+  //   SYSCALL_ARG1(r) = SYSCALL_ARG4(r);
+  // }
+  // return NULL;
+
+  SYSCALL_ARG1(r) = fs_write(fd, buf, len);
+  return NULL;
+}
+
+static inline _RegSet* sys_open(_RegSet *r) {
+  const char* pathname = (const char*)SYSCALL_ARG2(r);
+  int flags = (int)SYSCALL_ARG3(r);
+  int mode = (int)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_open(pathname,flags,mode);
+  return NULL;
+}
+static inline _RegSet* sys_read(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG2(r);
+  char *buf = (char *)SYSCALL_ARG3(r);
+  int len = (int)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_read(fd,buf,len);
+  return NULL;
+}
+static inline _RegSet* sys_close(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG2(r);
+  SYSCALL_ARG1(r) = fs_close(fd);
+  return NULL;
+}
+static inline _RegSet* sys_lseek(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG2(r);
+  off_t offset = (off_t)SYSCALL_ARG3(r);
+  int whence = (int)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_lseek(fd,offset,whence);
+  return NULL;
+}
+
+_RegSet* sys_exit(_RegSet *r){
+  _halt(SYSCALL_ARG2(r));
+  return NULL;
+}
+
+_RegSet* sys_brk(_RegSet *r){
+  // SYSCALL_ARG1(r) = 0;
+  SYSCALL_ARG1(r) = mm_brk(SYSCALL_ARG2(r));
+  return NULL;
 }
 
 _RegSet* do_syscall(_RegSet *r) {
-  uintptr_t a[4]; // 用于存储系统调用参数
-  a[0] = SYSCALL_ARG1(r);// 获取系统调用号，根据之前的实现，存储在eax中
+  uintptr_t a[4];
+  a[0] = SYSCALL_ARG1(r);
+  // printf("Now handling syscall ID = %d\n", a[0]);
 
   switch (a[0]) {
     case SYS_none:{
-    //SYS_none 是一个系统调用的标识符
-    //通常用于表示一个不执行任何操作的系统调用。返回值置1，return null
-      SYSCALL_ARG1(r) = 1;
+      return sys_none(r);
       break;
     }
-    case SYS_exit: {
-      // 获取退出状态参数
-      uintptr_t exit_status = SYSCALL_ARG2(r);
-      _halt(exit_status);  // 调用 _halt() 以退出
+    case SYS_open:{
+      return sys_open(r);
       break;
     }
-    case SYS_write: {
-      int fd = SYSCALL_ARG2(r);  // 获取文件描述符
-      const char *buf = (const char *)SYSCALL_ARG3(r);  // 获取缓冲区地址
-      size_t len = SYSCALL_ARG4(r);  // 获取写入长度
-      // if (fd == 1 || fd == 2) {  // 如果是 stdout 或 stderr
-      //   for (size_t i = 0; i < len; i++) {
-      //     _putc(buf[i]);  // 使用 _putc 输出字符
-      //   }
-      //    SYSCALL_ARG1(r) = len;  // 返回写入的字节数
-      // } 
-      Log("fs_write: fd=%d, buf=%p, len=%d", fd, buf, len);
-
-      SYSCALL_ARG1(r) = fs_write(fd,buf,len);
-      return NULL;
+    case SYS_read:{
+      return sys_read(r);
       break;
     }
-    case SYS_brk: {
-      uintptr_t addr = SYSCALL_ARG2(r);
-      SYSCALL_ARG1(r) = sys_brk(addr);
+    case SYS_write:{
+      return sys_write(r);
       break;
     }
-    case SYS_open: {
-      const char *pathname = (const char *)SYSCALL_ARG2(r);
-      int flags = SYSCALL_ARG3(r);
-      int mode = SYSCALL_ARG4(r);
-      
-      Log("syscall: open('%s', %d, %d)", pathname, flags, mode);
-      SYSCALL_ARG1(r) = fs_open(pathname, flags, mode);
+    case SYS_exit:{
+      return sys_exit(r);
       break;
     }
-    case SYS_read: {
-      int fd = SYSCALL_ARG2(r);
-      void *buf = (void *)SYSCALL_ARG3(r);
-      size_t count = SYSCALL_ARG4(r);
-      
-      SYSCALL_ARG1(r) = fs_read(fd, buf, count);
+    case SYS_close:{
+      return sys_close(r);
       break;
     }
-    case SYS_lseek: {
-      int fd = SYSCALL_ARG2(r);
-      off_t offset = SYSCALL_ARG3(r);
-      int whence = SYSCALL_ARG4(r);
-      
-      Log("syscall: lseek(%d, %d, %d)", fd, offset, whence);
-      SYSCALL_ARG1(r) = fs_lseek(fd, offset, whence);
+    case SYS_lseek:{
+      return sys_lseek(r);
       break;
     }
-    case SYS_close: {
-      int fd = SYSCALL_ARG2(r);
-      
-      Log("syscall: close(%d)", fd);
-      SYSCALL_ARG1(r) = fs_close(fd);
+    case SYS_brk:{
+      return sys_brk(r);
       break;
     }
     default: panic("Unhandled syscall ID = %d", a[0]);
