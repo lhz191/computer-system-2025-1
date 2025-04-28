b6441bac58c732e1d0b60cfffec4a49ee75704d1
tracer-ics2017 <tracer@njuics.org>
2025-04-27 19:38:13 -0700

    >  run 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  19:38:13 up 38 min,  1 user,  load average: 9.29, 7.81, 5.53 578699e318bb90bb51f0321e4086ef095835c575

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -33,15 +33,15 @@ _RegSet* do_syscall(_RegSet *r) {
       int fd = SYSCALL_ARG2(r);  // 获取文件描述符
       const char *buf = (const char *)SYSCALL_ARG3(r);  // 获取缓冲区地址
       size_t len = SYSCALL_ARG4(r);  // 获取写入长度
-      // if (fd == 1 || fd == 2) {  // 如果是 stdout 或 stderr
-      //   for (size_t i = 0; i < len; i++) {
-      //     _putc(buf[i]);  // 使用 _putc 输出字符
-      //   }
-      //    SYSCALL_ARG1(r) = len;  // 返回写入的字节数
-      // } 
+      if (fd == 1 || fd == 2) {  // 如果是 stdout 或 stderr
+        for (size_t i = 0; i < len; i++) {
+          _putc(buf[i]);  // 使用 _putc 输出字符
+        }
+         SYSCALL_ARG1(r) = len;  // 返回写入的字节数
+      } 
       Log("fs_write: fd=%d, buf=%p, len=%d", fd, buf, len);
 
-      SYSCALL_ARG1(r) = fs_write(fd,buf,len);
+      // SYSCALL_ARG1(r) = fs_write(fd,buf,len);
       break;
     }
     case SYS_brk: {
