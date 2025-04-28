fb8e6cb71b4e2501f5915e7bb8575fa1a25004f2
tracer-ics2017 <tracer@njuics.org>
2025-04-27 20:38:07 -0700

    >  compile 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  20:38:07 up  1:38,  1 user,  load average: 4.45, 3.39, 2.42 a8c958491f98ab1b1f8fd3a31b14eabdeb3b57d

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -151,11 +151,9 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   //   // 将高位全部置为1
   //   *dest |= (~0u) << (width * 8);
   // }
-  // rtl_li(&t1,32-width*8);
-  // rtl_shl(dest,src1,&t1);
-  // rtl_sar(dest,dest,&t1);
-  int shift = (8*(4-width));
-  *dest = ((int32_t)(*src1<<shift))>>shift;
+  rtl_li(&t1,32-width*8);
+  rtl_shl(dest,src1,&t1);
+  rtl_sar(dest,dest,&t1);
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
