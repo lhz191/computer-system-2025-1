56e0a8eb7f34c8e63bc2cedef46ca68e0a153e5a
tracer-ics2017 <tracer@njuics.org>
2025-04-27 19:10:55 -0700

    >  run 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  19:10:55 up 11 min,  1 user,  load average: 1.87, 1.39, 0.76 ac642073a01ba6fdee625f2d492901b68f3af3dd

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,8 +5,12 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
     case _EVENT_SYSCALL:
       return do_syscall(r);
     case _EVENT_TRAP:
-      printf("BAD TRAP: %d\n", e.cause);
-      panic("BAD TRAP");
+     printf("BAD TRAP: %d at eip = 0x%x\n", e.cause, r->eip);
+     // 打印寄存器状态
+     printf("eax = 0x%x, ebx = 0x%x, ecx = 0x%x, edx = 0x%x\n", 
+            r->eax, r->ebx, r->ecx, r->edx);
+     // ...然后再panic
+     panic("BAD TRAP");
       break;
     default: panic("Unhandled event ID = %d", e.event);
   }
