71d51396f8ba97281d6fd5585ea367aa137473d5
tracer-ics2017 <tracer@njuics.org>
2025-04-27 19:44:12 -0700

    >  run 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  19:44:12 up 44 min,  1 user,  load average: 3.21, 4.45, 4.75 e21ac3bf7adcbeddccc101aa8c89ecdd1e61f90

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -32,9 +32,9 @@ int main() {
 
   // uint32_t entry = loader(NULL, "/bin/text");
   // /bin/bmptest
-  uint32_t entry = loader(NULL, "/bin/bmptest");
+  // uint32_t entry = loader(NULL, "/bin/bmptest");
   // /bin/events
-  // uint32_t entry = loader(NULL, "/bin/events");
+  uint32_t entry = loader(NULL, "/bin/events");
 //   // /bin/pal
 // uint32_t entry = loader(NULL, "/bin/pal");
   ((void (*)(void))entry)();
