4419f8d8ea617699a66854439ea918fc3b085b48
2212478-Liu Hao Ze <2212478@mail.nankai.edu.cn>
2025-04-29 04:28:51 -0700

    PA4.1 mov_r2cr函数

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -17,20 +17,25 @@ make_EHelper(lidt) {
   print_asm_template1(lidt);
 }
 
+/* PA4.1: 实现CR寄存器的操作指令 */
 make_EHelper(mov_r2cr) {
-  TODO();
-
+  // 将通用寄存器的值移动到控制寄存器
+  switch (id_dest->reg) {
+    case 0: cpu.cr0 = id_src->val; break;
+    case 3: cpu.cr3 = id_src->val; break;
+    default: assert(0);
+  }
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
-  TODO();
-
+  // 将控制寄存器的值移动到通用寄存器
+  switch (id_src->reg) {
+    case 0: operand_write(id_dest, &cpu.cr0); break;
+    case 3: operand_write(id_dest, &cpu.cr3); break;
+    default: assert(0);
+  }
   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
-
-#ifdef DIFF_TEST
-  diff_test_skip_qemu();
-#endif
 }
 
 
