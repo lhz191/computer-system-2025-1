b8dc292e740cc5eac5033684fa3b3eb7a87e7883
tracer-ics2017 <tracer@njuics.org>
2025-05-01 11:22:42 -0700

    >  run 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  11:22:42 up  4:11,  1 user,  load average: 4.32, 2.21, 1.65 44e3382e536638a1d7448986d42931e304a354d

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -16,24 +16,6 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uint32_t new_brk) {
-  if(current->cur_brk == 0){
-    current->cur_brk = current -> max_brk = new_brk;
-  }
-  else{
-    if(new_brk > current -> max_brk){
-      // TODO: map memory region [current->max_brk, new_brk)
-      // into address space current->as
-      uintptr_t vaddr = (((uint32_t)(current->max_brk) + 0xfff) & 0xfffff000);
-      while(vaddr < new_brk){
-        _map(&current->as, (void*)vaddr, (void*)new_page());
-        vaddr += PGSIZE;
-      }
-      
-      current->max_brk = new_brk;
-    }
-
-    current->max_brk = new_brk;
-  }
   return 0;
 }
 
@@ -42,5 +24,4 @@ void init_mm() {
   Log("free physical pages starting from %p", pf);
 
   _pte_init(new_page, free_page);
-  Log("finish pte_init..");
 }
