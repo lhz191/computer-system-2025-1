15b675f980f59f7fada6e8e039ac5051e3ae53f4
tracer-ics2017 <tracer@njuics.org>
2025-05-01 11:28:12 -0700

    >  run 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  11:28:12 up  4:17,  1 user,  load average: 5.56, 4.11, 2.66 4281a1214e3b1722cf016f7071d8f0e565498ae0

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -13,24 +13,35 @@ void* new_page(void) {
 void free_page(void *p) {
   panic("not implement yet");
 }
-#define K4(va) (((uint32_t)(va)+0xfff) & ~0xfff)
+
 /* The brk() system call handler. */
 int mm_brk(uint32_t new_brk) {
-    if(current->cur_brk==0)  current->cur_brk=current->max_brk=new_brk;
-    else{
-        if(new_brk>current->max_brk)
-        {
-            uintptr_t va=K4(current->max_brk);
-            while(va<new_brk)
-            {
-                void *pa=new_page();
-                _map(&current->as,(void*)va,pa);
-                va+=PGSIZE;
-            }
-            current->max_brk=new_brk;
-        }
-        current->cur_brk=new_brk;
+  if (current->cur_brk == 0) {
+    current->cur_brk = current->max_brk = new_brk;
+  }
+  else {
+    if (new_brk > current->max_brk) {
+      // 计算需要映射的起始和结束地址（页对齐）
+      uint32_t first = PGROUNDUP(current->max_brk);
+      uint32_t end = PGROUNDDOWN(new_brk);
+      
+      // 如果new_brk刚好页对齐，需要特殊处理
+      if ((new_brk & 0xfff) == 0) {
+        end = PGSIZE;
+      }
+      
+      // 映射内存区域，一次一页
+      for (uint32_t va = first; va <= end; va += PGSIZE) {
+        void* pa = new_page();
+        _map(&(current->as), (void*)va, pa);
+      }
+      
+      current->max_brk = new_brk;
     }
+
+    current->cur_brk = new_brk;
+  }
+
   return 0;
 }
 
