13e6739efcc5e61e853dbfb761e6d70534d2ac9a
2212478-Liu Hao Ze <2212478@mail.nankai.edu.cn>
2025-05-02 02:11:44 -0700

    Pa4.3 添加时钟中断

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -9,8 +9,13 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
       do_syscall(r);
       return schedule(r);
     case _EVENT_TRAP:
+      
       printf("Received kernel trap, initiating context switch.\n");
       // Schedule will switch to the first user process
+      return schedule(r);
+    case _EVENT_IRQ_TIME:
+      // Pa4.3 处理时钟中断，进行进程调度
+      printf("Timer interrupt, initiating context switch.\n");
       return schedule(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
