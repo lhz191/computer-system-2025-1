f39e665326ab50bc6592b5df59ca0976b8e03830
tracer-ics2017 <tracer@njuics.org>
2025-05-01 19:55:15 -0700

    >  run 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  19:55:15 up 12:44,  1 user,  load average: 5.32, 4.57, 4.54 24d63690d79448cf58ebe15ef92447143c1835c4

--- nexus-am/am/arch/x86-nemu/src/pte.c
+++ nexus-am/am/arch/x86-nemu/src/pte.c
@@ -67,6 +67,7 @@ void _switch(_Protect *p) {
 }
 
 void _map(_Protect *p, void *va, void *pa) {
+  // printf("1111");
   // Pa4.1: 实现虚拟地址到物理地址的映射
   PDE *pgdir = (PDE*)p->ptr;  // 获取页目录基址
   
@@ -88,8 +89,8 @@ void _map(_Protect *p, void *va, void *pa) {
     pgdir[pde_idx] = (uint32_t)ptab | PTE_P | PTE_W | PTE_U;
   }
   
-  // 获取页表地址
-  PTE *ptab = (PTE*)(pgdir[pde_idx] & ~0xFFF);  // 清除低12位标志位得到页表基址
+  // 获取页表地址 - 使用PTE_ADDR宏替代手动掩码
+  PTE *ptab = (PTE*)PTE_ADDR(pgdir[pde_idx]);
   
   // 更新页表项，建立va到pa的映射
   ptab[pte_idx] = (uint32_t)pa | PTE_P | PTE_W | PTE_U;
