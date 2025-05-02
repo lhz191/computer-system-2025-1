130f289355174c028cce503606271cab1d6ef207
tracer-ics2017 <tracer@njuics.org>
2025-05-01 20:09:51 -0700

    >  run 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  20:09:51 up 12:58,  1 user,  load average: 5.12, 4.71, 4.71 ad49b908589e134e556dd50e7b4dea225d6c441

--- nexus-am/am/arch/x86-nemu/src/pte.c
+++ nexus-am/am/arch/x86-nemu/src/pte.c
@@ -67,7 +67,6 @@ void _switch(_Protect *p) {
 }
 
 void _map(_Protect *p, void *va, void *pa) {
-  // printf("1111");
   // Pa4.1: 实现虚拟地址到物理地址的映射
   PDE *pgdir = (PDE*)p->ptr;  // 获取页目录基址
   
@@ -75,10 +74,11 @@ void _map(_Protect *p, void *va, void *pa) {
   uint32_t pde_idx = PDX(va);  // 取虚拟地址的高10位作为页目录索引
   uint32_t pte_idx = PTX(va);  // 取虚拟地址的中10位作为页表索引
   
+  PTE *ptab;
   // 检查页目录项是否存在，不存在则创建一个新的页表
   if (!(pgdir[pde_idx] & PTE_P)) {
     // 申请一个新页用作页表
-    PTE *ptab = (PTE*)palloc_f();
+    ptab = (PTE*)palloc_f();
     
     // 初始化页表，将所有页表项设为无效
     for (int i = 0; i < NR_PTE; i++) {
@@ -87,10 +87,10 @@ void _map(_Protect *p, void *va, void *pa) {
     
     // 更新页目录项，指向新的页表，并设置存在位和读写位
     pgdir[pde_idx] = (uint32_t)ptab | PTE_P | PTE_W | PTE_U;
+  } else {
+    // 页目录项已存在，获取页表地址
+    ptab = (PTE*)(pgdir[pde_idx] & ~0xFFF);  // 清除低12位标志位得到页表基址
   }
-  
-  // 获取页表地址 - 使用PTE_ADDR宏替代手动掩码
-  PTE *ptab = (PTE*)PTE_ADDR(pgdir[pde_idx]);
   
   // 更新页表项，建立va到pa的映射
   ptab[pte_idx] = (uint32_t)pa | PTE_P | PTE_W | PTE_U;
