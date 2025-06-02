ca12d44ff242c2dc7eacbf962046a473ef03c0f6
tracer-ics2017 <tracer@njuics.org>
2025-06-02 02:57:38 -0700

    >  compile 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  02:57:38 up  4:30,  1 user,  load average: 2.29, 2.30, 2.33 8790a21d1ce2e7bc57fc8cafe32da56f232e2f33

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -261,13 +261,6 @@ make_DHelper(gp2_cl2E) {
   sprintf(id_src->str, "%%cl");
 #endif
 }
-
-make_DHelper(gp2_Ib2E) {
-  decode_op_rm(eip, id_dest, true, NULL, false);
-  id_src->width = 1;
-  decode_op_I(eip, id_src, true);
-}
-
 /* Ev <- GvIb
  * use for shld/shrd */
 /*Pa5: SHLD/SHRD instructions:
@@ -277,6 +270,11 @@ make_DHelper(gp2_Ib2E) {
  * - count is an 8-bit immediate
  * - Gv is the source operand for fill bits
  */
+make_DHelper(gp2_Ib2E) {
+  decode_op_rm(eip, id_dest, true, NULL, false);
+  id_src->width = 1;
+  decode_op_I(eip, id_src, true);
+}
 
 make_DHelper(O2a) {
   decode_op_O(eip, id_src, true);
