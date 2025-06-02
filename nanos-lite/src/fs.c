d5a50f300244e212a05ad308c9afbab6fe84ba87
tracer-ics2017 <tracer@njuics.org>
2025-06-02 04:15:20 -0700

    >  run 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  04:15:20 up  5:48,  1 user,  load average: 6.22, 5.89, 6.08 bf699eee183e302fd8298040168ff5ded1ab3af0

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -104,7 +104,7 @@ ssize_t fs_write(int fd, const void *buf, size_t len) {
   // 检查文件描述符是否有效
   assert(fd >= 0 && fd < NR_FILES);
   
-  Log("fs_write: fd=%d, buf=%p, len=%d", fd, buf, len);
+  // Log("fs_write: fd=%d, buf=%p, len=%d", fd, buf, len);
   //1.stdout：将数据输出到串口或控制台。
   //2.stderr：用于标准错误输出。
   //3./dev/fb：用于写入帧缓冲区。写入的数据通常是图像数据，需要按照特定的格式（如 RGB）写入帧缓冲区。
