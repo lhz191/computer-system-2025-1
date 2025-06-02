51493cdbd112b9d22b03f035f33d67f36e20cc2f
tracer-ics2017 <tracer@njuics.org>
2025-06-02 08:34:27 -0700

    >  run 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  08:34:27 up 10:07,  1 user,  load average: 14.34, 14.61, 14.47 71058e41cf18bfee9be72b24af32eb3c885e5b5

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -40,7 +40,7 @@ int main() {
 // uint32_t entry = loader(NULL, "/bin/pal");
 //   ((void (*)(void))entry)();
 load_prog("/bin/pal");  // Pa4.1: 使用load_prog加载用户程序到独立的虚拟地址空间
-load_prog("/bin/hello");
+// load_prog("/bin/hello");
 // load_prog("/bin/videotest");
 
 _trap();  // Use kernel self-trap to switch to user process
