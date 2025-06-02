b91ca751d4b70678616d9e32822778539454113a
tracer-ics2017 <tracer@njuics.org>
2025-06-02 05:40:11 -0700

    >  compile 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  05:40:11 up  7:13,  1 user,  load average: 10.94, 11.20, 10.59 9ffba5787dd25fd318aabae8496c5b531b1d4459

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -208,8 +208,8 @@ opcode_entry opcode_table [512] = {
   /* 0xd0 */	IDEXW(gp2_1_E, gp2, 1), IDEX(gp2_1_E, gp2), IDEXW(gp2_cl2E, gp2, 1), IDEX(gp2_cl2E, gp2),
   /* 0xd4 */	EMPTY, EMPTY, EX(nemu_trap), EMPTY,
   /*Pa5：0xd8 fld指令*/
-  /* 0xd8 */	EMPTY, IDEX(E, fld), EMPTY, EMPTY,
-  /*Pa5:0xd fld指令*/
+  /* 0xd8 */	EMPTY, IDEX(E, fld), IDEX(E, fld), EMPTY,
+  /*Pa5:0xdd fld指令*/
   /* 0xdc */	EMPTY, IDEX(E, fld), EMPTY, EMPTY,
   /* 0xe0 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0xe4 */	EMPTY, EMPTY, EMPTY, EMPTY,
