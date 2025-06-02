c93567c8429db76a63e883665aa02fbff25e579d
tracer-ics2017 <tracer@njuics.org>
2025-06-02 03:53:57 -0700

    >  compile 2212478 lhz Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  03:53:57 up  5:26,  1 user,  load average: 4.84, 6.18, 6.12 69363deb8c738ed521e08a80c376bc2da907007f

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -149,11 +149,12 @@ make_EHelper(not) {
 make_EHelper(shld) {
   // SHLD指令：将目标操作数（第一个操作数）向左移位，
   // 空出的低位用第二个操作数的对应位填充
-  rtl_shl(&t0, &id_dest->val, &id_src->val);  // 先左移
-  rtl_shri(&t1, &id_src2->val, 32 - id_src->val);  // 准备填充位
-  rtl_or(&t0, &t0, &t1);  // 合并结果
+  rtl_shl(&t1,&id_dest->val, &id_src->val);
+  rtl_li(&t3,id_src2->width << 3);
+  rtl_sub(&t3,&t3,&id_src->val);
+  rtl_shr(&t2,&id_src2->val, &t3);
+  rtl_or(&t0,&t1,&t2);
   operand_write(id_dest, &t0);
-
   // 更新标志位
   rtl_update_ZFSF(&t0, id_dest->width);
   
@@ -163,9 +164,11 @@ make_EHelper(shld) {
 make_EHelper(shrd) {
   // SHRD指令：将目标操作数（第一个操作数）向右移位，
   // 空出的高位用第二个操作数的对应位填充
-  rtl_shr(&t0, &id_dest->val, &id_src->val);  // 先右移
-  rtl_shli(&t1, &id_src2->val, 32 - id_src->val);  // 准备填充位
-  rtl_or(&t0, &t0, &t1);  // 合并结果
+  rtl_shr(&t1,&id_dest->val, &id_src->val);
+  rtl_li(&t3,id_src2->width << 3);
+  rtl_sub(&t3,&t3, &id_src->val);
+  rtl_shl(&t2, &id_src2->val, &t3);
+  rtl_or(&t0, &t1, &t2);  // 合并结果
   operand_write(id_dest, &t0);
 
   // 更新标志位
